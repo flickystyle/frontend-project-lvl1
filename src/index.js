6e5321e1c50c996fd1c4e98910fa9f6498b7cd74
Dmitry Kokorev <superflick3@gmail.com>
2021-11-14 12:02:58 +0300

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ const game = (description, getQuestionAnswer) => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
+  const gameRounds = 3;
 
-  for (let i = 1; i < 4; i += 1) {
-    const [gameQuestion, gameAnswer] = getQuestionAnswer();
+  for (let i = 0; i < gameRounds; i += 1) {
+    const [gameQuestion, gameCorrectAnswer] = getQuestionAnswer();
     console.log(`Question: ${gameQuestion}`);
     const userAnswer = readlineSync.question('You answer: ');
 
-    if (gameAnswer === userAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${gameAnswer}'.`);
+    if (gameCorrectAnswer !== userAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${gameCorrectAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
-      return;
     }
-  } console.log(`Congratulations, ${userName}!`);
+    console.log('Correct');
+  }
+  return console.log(`Congratulations, ${userName}!`);
 };
 
 export default game;
