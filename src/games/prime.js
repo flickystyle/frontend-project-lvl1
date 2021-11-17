a6157ff351b5059dbdea47f8d16c603c76cb0652
Dmitry Kokorev <superflick3@gmail.com>
2021-11-17 09:58:34 +0300

    Fix prime.js guard-check first

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,12 +2,15 @@ import game from '../index.js';
 import getRandomNumber from '../random_number.js';
 
 const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
-  return num > 1;
+  return true;
 };
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
